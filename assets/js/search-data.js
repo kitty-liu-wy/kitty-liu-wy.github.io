255b8fe233f146e385064ba0fc0f0455393e51c9
kitty-liu-wy <kitty.liu.wy@outlook.com>
2026-01-22 14:19:24 +0000

    Deploying to gh-pages from @ kitty-liu-wy/kitty-liu-wy.github.io@0833ef19ae6ef11973fc2b5cec5dbb824b474520 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications_presentations/";
           },
+        },{id: "nav-teaching-amp-service",
+          title: "teaching &amp; service",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching_service/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
