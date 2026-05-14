e4844349c3bea921b154623ca2413e38cb15f8ba
kitty-liu-wy <kitty.liu.wy@outlook.com>
2026-05-14 12:24:20 +0000

    Deploying to gh-pages from @ kitty-liu-wy/kitty-liu-wy.github.io@328f4d89883a01a12ff3c95fb1fc20445df3b715 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-presentations",
+          title: "presentations",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/presentations/";
+          },
         },{id: "nav-teaching-amp-service",
           title: "teaching &amp; service",
           description: "",
